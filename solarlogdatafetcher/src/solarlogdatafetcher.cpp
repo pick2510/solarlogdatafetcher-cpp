78f207319619083a7bf31ade81f9a4e9cca8f1a9
Dominik Strebel <dominik.strebel@gmail.com>
2016-08-02 10:10:00 +0200

    Change description

--- solarlogdatafetcher/src/solarlogdatafetcher.cpp
+++ solarlogdatafetcher/src/solarlogdatafetcher.cpp
@@ -3,7 +3,7 @@
 // Author      : Dominik Strebel
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : 
 //============================================================================
 
 #define MAXLINES 30
